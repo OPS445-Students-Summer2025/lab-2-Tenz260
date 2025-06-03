4e9420029c2cce8cdc26810cce1e5bafb8d99302
Tenzin wangyel <twangyel@myseneca.ca>
2025-06-03 06:01:53 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Tenzin Wangyel
+# Author ID: 134207232
+# Date Created: 2025/06/03
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
